3091fa7c555d224b4c72441f981241ad23a4878e
omust <omust2023@gmail.com>
2024-04-30 10:42:57 +0900

    deploy to vercel

---
+++ app/darkmode/page.tsx
@@ -0,0 +1,6 @@
+import { Component } from "@/components/darkmode";
+import Image from "next/image";
+
+export default function Page() {
+  return <Component />;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,41 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
+
+/** @type {import('tailwindcss').Config} */
+//tailwind.config.js
+//FreeBSD-licensed CSS animation by Animista
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        "tracking-in-contract":
+          "tracking-in-contract 3.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.5s   both",
+      },
+      keyframes: {
+        "tracking-in-contract": {
+          "0%": {
+            "letter-spacing": "1em",
+            opacity: "0",
+          },
+          "40%": {
+            opacity: ".6",
+          },
+          to: {
+            "letter-spacing": "normal",
+            opacity: "1",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
